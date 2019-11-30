aabfe9d44df6760bfd0588e06569cf1b1eb5ea93
Nahuel <nahueldolores@hotmail.com>
2019-11-30 19:08:12 -0300

    Changed how it works for the better, Retrofit calls are wack

--- src/main/java/me/nahu/launchlibrary/LaunchLibrary.java
+++ src/main/java/me/nahu/launchlibrary/LaunchLibrary.java
@@ -1,7 +1,23 @@
 package me.nahu.launchlibrary;
 
+import me.nahu.launchlibrary.entities.TypeQuery;
+import me.nahu.launchlibrary.entities.agency.AgencyQuery;
+import me.nahu.launchlibrary.entities.agency.AgencyType;
+import me.nahu.launchlibrary.entities.event.EventType;
+import me.nahu.launchlibrary.entities.launch.LaunchQuery;
+import me.nahu.launchlibrary.entities.launch.LaunchStatus;
+import me.nahu.launchlibrary.entities.location.LocationQuery;
+import me.nahu.launchlibrary.entities.location.PadQuery;
+import me.nahu.launchlibrary.entities.mission.MissionQuery;
+import me.nahu.launchlibrary.entities.mission.MissionType;
+import me.nahu.launchlibrary.entities.rocket.RocketQuery;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.Date;
+import java.util.List;
+
+import static me.nahu.launchlibrary.util.Utils.DATE_FORMAT;
 
 public class LaunchLibrary {
     private static final Version DEFAULT_API_VERSION = Version.VERSION_1_4_1;
@@ -18,10 +34,242 @@ public class LaunchLibrary {
         launchService = retrofit.create(LaunchService.class);
     }
 
-    private LaunchService getLaunchService() {
-        return launchService;
+    /**
+     * Get the agency by the unique id assigned by the API.
+     * @param id {@link Integer} id.
+     * @return {@link Request} of type {@link AgencyQuery.Agency}.
+     */
+    public Request<AgencyQuery> getAgencyById(int id) {
+        return new Request<>(launchService.getAgency(String.valueOf(id)));
     }
 
+    /**
+     * Get the agency by its abbreviation, like NASA.
+     * @param abbr {@link String} abbreviation.
+     * @return {@link Request} of type {@link AgencyQuery.Agency}.
+     */
+    public Request<AgencyQuery> getAgencyByAbbreviation(String abbr) {
+        return new Request<>(launchService.getAgency(abbr));
+    }
+
+    /**
+     * Get the agency by its name, like Belarus Space Agency.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link AgencyQuery.Agency}.
+     */
+    public Request<AgencyQuery> getAgenciesByName(String name) {
+        return new Request<>(launchService.getAgenciesByName(name));
+    }
+
+    /**
+     * Get the agency type by the unique id assigned by the API.
+     * @param id {@link Integer} id.
+     * @return {@link Request} of type {@link AgencyType}.
+     */
+    public Request<TypeQuery<AgencyType>> getAgencyTypeById(int id) {
+        return new Request<>(launchService.getAgencyType(String.valueOf(id)));
+    }
+
+    /**
+     * Get the agency types by their name, like Government.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link AgencyType}.
+     */
+    public Request<TypeQuery<AgencyType>> getAgencyTypesByName(String name) {
+        return new Request<>(launchService.getAgencyType(name));
+    }
+
+    /**
+     * Get the event type by the unique id assigned by the API.
+     * @param id {@link Integer} id.
+     * @return {@link Request} of type {@link EventType}
+     */
+    public Request<TypeQuery<EventType>> getEventTypeById(int id) {
+        return new Request<>(launchService.getEventType(String.valueOf(id)));
+    }
+
+    /**
+     * Get the event types by their name, like Info.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link EventType}.
+     */
+    public Request<TypeQuery<EventType>> getEventTypesByName(String name) {
+        return new Request<>(launchService.getEventType(name));
+    }
+
+    /**
+     * Get the launch by the unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link LaunchQuery.Launch}
+     */
+    public Request<LaunchQuery> getLaunchById(int id) {
+        return new Request<>(launchService.getLaunch(String.valueOf(id)));
+    }
+
+    /**
+     * Get launches according by their name, like querying Falcon.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link LaunchQuery.Launch}.
+     */
+    public Request<LaunchQuery> getLaunchesByName(String name) {
+        return new Request<>(launchService.getLaunch(name));
+    }
+
+    /**
+     * Get next amount of launches.
+     * @param amount {@link Integer} amount of wanted launches.
+     * @return {@link Request} of type {@link List} of {@link LaunchQuery.Launch}.
+     */
+    public Request<LaunchQuery> getNextLaunches(int amount) {
+        return new Request<>(launchService.getNextLaunches(amount));
+    }
+
+    /**
+     * Get launches after a provided convenient {@link Date} class.
+     * @param moment {@link Date} date.
+     * @return {@link Request} of type {@link List} of {@link LaunchQuery.Launch}.
+     */
+    public Request<LaunchQuery> getLaunchesAfter(Date moment) {
+        return new Request<>(launchService.getLaunch(DATE_FORMAT.format(moment)));
+    }
+
+    /**
+     * Get launches between two ISO dates.
+     * @param from {@link String} formatted YYYY-MM-DD.
+     * @param to {@link String} formatted YYYY-MM-DD.
+     * @return {@link Request} of type {@link List} of {@link LaunchQuery.Launch}.
+     */
+    public Request<LaunchQuery> getLaunchesBetween(Date from, Date to) {
+        return new Request<>(launchService.getLaunch(DATE_FORMAT.format(from) + "/" + DATE_FORMAT.format(to)));
+    }
+
+    /**
+     * Get launch status by the unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link LaunchStatus}
+     */
+    public Request<TypeQuery<LaunchStatus>> getLaunchStatusById(int id) {
+        return new Request<>(launchService.getLaunchStatus(String.valueOf(id)));
+    }
+
+    /**
+     * Get launch statuses matching a name, like On Hold.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link LaunchStatus}.
+     */
+    public Request<TypeQuery<LaunchStatus>> getLaunchStatusesByName(String name) {
+        return new Request<>(launchService.getLaunchStatus(name));
+    }
+
+    /**
+     * Get location by the unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link LocationQuery.Location}
+     */
+    public Request<LocationQuery> getLocationFromId(int id) {
+        return new Request<>(launchService.getLocation(String.valueOf(id), null));
+    }
+
+    /**
+     * Get locations from their name, like Cape Canaveral, Florida
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link LocationQuery.Location}.
+     */
+    public Request<LocationQuery> getLocationsFromName(String name) {
+        return new Request<>(launchService.getLocation(name, null));
+    }
+
+
+    /**
+     * Get locations from their 3-letter country code, like USA or CHN.
+     * @param countryCode {@link String} name.
+     * @return {@link Request} of type {@link LocationQuery.Location}.
+     */
+    public Request<LocationQuery> getLocationsFromCountryCode(String countryCode) {
+        return new Request<>(launchService.getLocation(null, countryCode));
+    }
+
+    /**
+     * Get a mission by its unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link MissionQuery.Mission}
+     */
+    public Request<MissionQuery> getMissionById(int id) {
+        return new Request<>(launchService.getMission(String.valueOf(id)));
+    }
+
+    /**
+     * Get missions from their name, like "Falcon" will return all Falcon missions.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link MissionQuery.Mission}.
+     */
+    public Request<MissionQuery> getMissionsByName(String name) {
+        return new Request<>(launchService.getMission(name));
+    }
+
+    /**
+     * Get a mission type by its unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link MissionType}
+     */
+    public Request<TypeQuery<MissionType>> getMissionFromId(int id) {
+        return new Request<>(launchService.getMissionType(String.valueOf(id)));
+    }
+
+    /**
+     * Get mission types from their name, like Earth Science.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link MissionType}.
+     */
+    public Request<TypeQuery<MissionType>> getMissionTypesFromName(String name) {
+        return new Request<>(launchService.getMissionType(name));
+    }
+
+    /**
+     * Get a pad by its unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link PadQuery.Pad}
+     */
+    public Request<PadQuery> getPadFromId(int id) {
+        return new Request<>(launchService.getPad(String.valueOf(id)));
+    }
+
+    /**
+     * Get pads from their name, like Cape Canaveral will return all pads in there.
+     * @param name {@link String} name.
+     * @return {@link Request} of type {@link PadQuery.Pad}.
+     */
+    public Request<PadQuery> getPadsFromName(String name) {
+        return new Request<>(launchService.getPad(name));
+    }
+
+    /**
+     * Get pad located in a certain place.
+     * @param location {@link LocationQuery.Location} location.
+     * @return {@link Request} of type {@link PadQuery.Pad}.
+     */
+    public Request<PadQuery> getPadsByLocation(LocationQuery.Location location) {
+        return new Request<>(launchService.getPadsByLocationId(location.getId()));
+    }
+
+    /**
+     * Get a rocket by its unique id assigned by the API.
+     * @param id {@link Integer} id
+     * @return {@link Request} of type {@link RocketQuery.Rocket}
+     */
+    public Request<RocketQuery> getRocketById(int id) {
+        return new Request<>(launchService.getRocket(String.valueOf(id)));
+    }
+
+    /**
+     * Get rockets by their name, Falcon will return all the series of Falcon rockets.
+     * @param name {@link String} name
+     * @return {@link Request} of type {@link RocketQuery.Rocket}
+     */
+    public Request<RocketQuery> getRocketsFromName(String name) {
+        return new Request<>(launchService.getRocket(name));
+    }
+    
     public static class Builder {
         private Version version = DEFAULT_API_VERSION;
         private String apiUrl = DEFAULT_API_URL;
@@ -50,8 +298,8 @@ public class LaunchLibrary {
          * Build an instance of {@link LaunchLibrary} with the properties set to this builder.
          * @return {@link LaunchLibrary} instance.
          */
-        public LaunchService build() {
-            return new LaunchLibrary(apiUrl, version).getLaunchService();
+        public LaunchLibrary build() {
+            return new LaunchLibrary(apiUrl, version);
         }
     }
 }
--- src/main/java/me/nahu/launchlibrary/LaunchService.java
+++ src/main/java/me/nahu/launchlibrary/LaunchService.java
@@ -16,16 +16,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-import java.util.Date;
 import java.util.List;
 
-import static me.nahu.launchlibrary.util.Utils.DATE_FORMAT;
-
 public interface LaunchService {
-    /*
-     *  AGENCY CALLS
-     */
-
     /**
      * Fetch an Agency.
      * @param param ambiguous parameter, could be id, abbreviation or name.
@@ -35,34 +28,12 @@ public interface LaunchService {
     Call<AgencyQuery> getAgency(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getAgency(String)} providing ease of use.
-     * @param id {@link Integer} id.
-     * @return {@link Call} of type {@link AgencyQuery.Agency}.
-     */
-    default Call<AgencyQuery> getAgencyById(int id) {
-        return getAgency(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getAgency(String)} providing ease of use.
-     * @param abbr {@link String} abbreviation.
-     * @return {@link Call} of type {@link AgencyQuery.Agency}.
-     */
-    default Call<AgencyQuery> getAgencyByAbbreviation(String abbr) {
-        return getAgency(abbr);
-    }
-
-    /**
      * Function that returns a list of all agencies with that name.
      * @param name {@link String} name.
      * @return {@link Call} of type {@link AgencyQuery.Agency}.
      */
-    @GET("agency/")
+    @GET("agency")
     Call<AgencyQuery> getAgenciesByName(@Query("name") String name);
-
-    /*
-     *  AGENCY TYPE CALLS
-     */
 
     /**
      * Fetch an AgencyType.
@@ -73,56 +44,12 @@ public interface LaunchService {
     Call<TypeQuery<AgencyType>> getAgencyType(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getAgencyType(String)} providing ease of use.
-     * @param id {@link Integer} id.
-     * @return {@link Call} of type {@link AgencyType}.
-     */
-    default Call<TypeQuery<AgencyType>> getAgencyTypeById(int id) {
-        return getAgencyType(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getAgencyType(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link AgencyType}.
-     */
-    default Call<TypeQuery<AgencyType>> getAgencyTypesByName(String name) {
-        return getAgencyType(name);
-    }
-
-    /*
-     *  EVENT TYPE CALLS
-     */
-
-    /**
      * Fetch an EventType
      * @param param ambiguous parameter, could be id, abbreviation or name
      * @return {@link Call} of type {@link EventType}
      */
     @GET("eventtype/{param}")
     Call<TypeQuery<EventType>> getEventType(@Path("param") String param);
-
-    /**
-     * Function that wraps {@link #getEventType(String)} providing ease of use.
-     * @param id {@link Integer} id.
-     * @return {@link Call} of type {@link EventType}
-     */
-    default Call<TypeQuery<EventType>> getEventTypeById(int id) {
-        return getEventType(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getEventType(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link EventType}.
-     */
-    default Call<TypeQuery<EventType>> getEventTypesByName(String name) {
-        return getEventType(name);
-    }
-
-    /*
-     *  LAUNCH CALLS
-     */
 
     /**
      * Fetch a Launch
@@ -133,70 +60,12 @@ public interface LaunchService {
     Call<LaunchQuery> getLaunch(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getLaunch(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link LaunchQuery.Launch}
-     */
-    default Call<LaunchQuery> getLaunchById(int id) {
-        return getLaunch(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getLaunch(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link LaunchQuery.Launch}.
-     */
-    default Call<LaunchQuery> getLaunchesByName(String name) {
-        return getLaunch(name);
-    }
-
-    /**
      * Get next amount of launches.
      * @param amount {@link Integer} amount of wanted launches.
      * @return {@link Call} of type {@link List} of {@link LaunchQuery.Launch}.
      */
     @GET("launch/next/{amount}")
     Call<LaunchQuery> getNextLaunches(@Path("amount") int amount);
-
-    /**
-     * Get launches after a provided ISO date.
-     * @param moment {@link String} formatted YYYY-MM-DD.
-     * @return {@link Call} of type {@link List} of {@link LaunchQuery.Launch}.
-     */
-    @GET("launch/{moment}")
-    Call<LaunchQuery> getLaunchesAfter(@Path("moment") String moment);
-
-    /**
-     * Get launches after a provided convenient {@link Date} class.
-     * @param moment {@link Date} date.
-     * @return {@link Call} of type {@link List} of {@link LaunchQuery.Launch}.
-     */
-    default Call<LaunchQuery> getLaunchesAfter(Date moment) {
-        return getLaunchesAfter(DATE_FORMAT.format(moment));
-    }
-
-    /**
-     * Get launches between two ISO dates.
-     * @param from {@link String} formatted YYYY-MM-DD.
-     * @param to {@link String} formatted YYYY-MM-DD.
-     * @return {@link Call} of type {@link List} of {@link LaunchQuery.Launch}.
-     */
-    @GET("launch/{from}/{to}")
-    Call<LaunchQuery> getLaunchesBetween(@Path("from") String from, @Path("to") String to);
-
-    /**
-     * Get launches after convenient {@link Date} classes.
-     * @param from {@link Date} date.
-     * @param to {@link Date} to.
-     * @return {@link Call} of type {@link List} of {@link LaunchQuery.Launch}.
-     */
-    default Call<LaunchQuery> getLaunchesBetween(Date from, Date to) {
-        return getLaunchesBetween(DATE_FORMAT.format(from), DATE_FORMAT.format(to));
-    }
-
-    /*
-     *  LAUNCH STATUS CALLS
-     */
 
     /**
      * Fetch a Launch Status
@@ -207,64 +76,12 @@ public interface LaunchService {
     Call<TypeQuery<LaunchStatus>> getLaunchStatus(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getLaunchStatus(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link LaunchStatus}
-     */
-    default Call<TypeQuery<LaunchStatus>> getLaunchStatusById(int id) {
-        return getLaunchStatus(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getLaunchStatus(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link LaunchStatus}.
-     */
-    default Call<TypeQuery<LaunchStatus>> getLaunchStatusesByName(String name) {
-        return getLaunchStatus(name);
-    }
-
-    /*
-     *  LOCATION CALLS
-     */
-
-    /**
      * Fetch a Location
      * @param param ambiguous parameter, could be id, abbreviation or name
      * @return {@link Call} of type {@link LocationQuery.Location}
      */
     @GET("location/{param}")
-    Call<LocationQuery> getLocation(@Path("param") String param);
-
-    /**
-     * Function that wraps {@link #getLocation(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link LocationQuery.Location}
-     */
-    default Call<LocationQuery> getLocationById(int id) {
-        return getLocation(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getLocation(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link LocationQuery.Location}.
-     */
-    default Call<LocationQuery> getLocationsByName(String name) {
-        return getLocation(name);
-    }
-
-    /**
-     * Get location by three letter country code the Location's country of origin.
-     * @param countryCode {@link String} three letter country code.
-     * @return {@link Call} of type {@link List} of {@link LocationQuery.Location}.
-     */
-    @GET("location/")
-    Call<LocationQuery> getLocationsByCountryCode(@Query("countryCode") String countryCode);
-
-    /*
-     *  MISSION CALLS
-     */
+    Call<LocationQuery> getLocation(@Path("param") String param, @Query("countryCode") String countryCode);
 
     /**
      * Fetch a Mission
@@ -275,56 +92,12 @@ public interface LaunchService {
     Call<MissionQuery> getMission(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getMission(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link MissionQuery.Mission}
-     */
-    default Call<MissionQuery> getMissionById(int id) {
-        return getMission(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getMission(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link MissionQuery.Mission}.
-     */
-    default Call<MissionQuery> getMissionsByName(String name) {
-        return getMission(name);
-    }
-
-    /*
-     *  MISSION TYPE CALLS
-     */
-
-    /**
      * Fetch a Mission Type
      * @param param ambiguous parameter, could be id, abbreviation or name
      * @return {@link Call} of type {@link MissionType}
      */
     @GET("missiontype/{param}")
     Call<TypeQuery<MissionType>> getMissionType(@Path("param") String param);
-
-    /**
-     * Function that wraps {@link #getMissionType(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link MissionType}
-     */
-    default Call<TypeQuery<MissionType>> getMissionTypeById(int id) {
-        return getMissionType(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getMissionType(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link MissionType}.
-     */
-    default Call<TypeQuery<MissionType>> getMissionTypesByName(String name) {
-        return getMissionType(name);
-    }
-
-    /*
-     *  PAD CALLS
-     */
 
     /**
      * Fetch a Pad
@@ -335,24 +108,6 @@ public interface LaunchService {
     Call<PadQuery> getPad(@Path("param") String param);
 
     /**
-     * Function that wraps {@link #getPad(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link PadQuery.Pad}
-     */
-    default Call<PadQuery> getPadById(int id) {
-        return getPad(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getPad(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link PadQuery.Pad}.
-     */
-    default Call<PadQuery> getPadsByName(String name) {
-        return getPad(name);
-    }
-
-    /**
      * Get pad by {@link LocationQuery.Location}'s id.
      * @param locationId {@link Integer} id.
      * @return {@link Call} of type {@link PadQuery.Pad}.
@@ -361,41 +116,10 @@ public interface LaunchService {
     Call<PadQuery> getPadsByLocationId(@Query("locationid") int locationId);
 
     /**
-     * Get pad by passing a {@link LocationQuery.Location}.
-     * @param location {@link LocationQuery.Location} location.
-     * @return {@link Call} of type {@link PadQuery.Pad}.
-     */
-    default Call<PadQuery> getPadByLocation(LocationQuery.Location location) {
-        return getPadsByLocationId(location.getId());
-    }
-
-    /*
-     *  ROCKET CALLS
-     */
-
-    /**
      * Fetch a Rocket
      * @param param ambiguous parameter, could be id, abbreviation or name
      * @return {@link Call} of type {@link RocketQuery.Rocket}
      */
     @GET("rocket/{param}")
     Call<RocketQuery> getRocket(@Path("param") String param);
-
-    /**
-     * Function that wraps {@link #getRocket(String)} providing ease of use.
-     * @param id {@link Integer} id
-     * @return {@link Call} of type {@link RocketQuery.Rocket}
-     */
-    default Call<RocketQuery> getRocketById(int id) {
-        return getRocket(String.valueOf(id));
-    }
-
-    /**
-     * Function that wraps {@link #getRocket(String)} providing ease of use.
-     * @param name {@link String} name.
-     * @return {@link Call} of type {@link RocketQuery.Rocket}.
-     */
-    default Call<RocketQuery> getRocketsByName(String name) {
-        return getRocket(name);
-    }
 }
